c83152fb91763a32d6ce9e6f84885d478fc1a92b
aigazou0-ui <aigazou0@gmail.com>
2026-06-05 04:40:33 +0900

    Bump shogi evaluation cache

--- js/ai-worker.js
+++ js/ai-worker.js
@@ -4,7 +4,7 @@ importScripts(
   "pieces.js?v=75",
   "board.js?v=75",
   "rules.js?v=80",
-  "evaluation.js?v=82",
+  "evaluation.js?v=83",
   "opening-book.js?v=1",
   "opening.js?v=85",
   "ai.js?v=89"
